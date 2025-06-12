0854a0770c62dde92b073b9be95eb07b96126933
erickromao <erickromao50@gmail.com>
2025-06-12 11:43:37 -0300

    Adicionado as migrations e conexao ao banco

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 
 using ProdutosAPi.Data;
-var builder = WebApplication.CreateBuilder(args); // Adicionar serviços ao container.
+var builder = WebApplication.CreateBuilder(args); // Adicionar serviços ao container. (Registrador de serviços)
 
 //Obtendo string de conexão
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-//Registrando ApplicationDbContext com o provedor Npgsql
+//Registrando ApplicationDbContext com o provedor Npgsql (COnfigurando para usar o postgres)
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); // Necessário para explorar os endpoints da API
 builder.Services.AddSwaggerGen();// Adiciona o gerador do Swagger
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -24,14 +21,9 @@ var app = builder.Build();
 // Configura o pipeline de requisição HTTP.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+    app.UseSwagger(); // Middleware para gerar o JSON da especificacao
+    app.UseSwaggerUI(); // Middleware para a intefarce gráfica do SWAGGER UI
+    app.MapOpenApi(); // Mapeia os endpoints para servir o arquivo JSON da especificacao
 }
 
 app.UseHttpsRedirection();
